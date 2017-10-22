574654f395c6400ad8036f488fa98148420a6575
s3p02 <sreesaisharan.pantangi@ucdenver.edu>
2017-10-22 02:24:54 -0600

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
+#!/bin/bash
 
+#For Ubuntu 14.04 uncomment the following 3 lines asnd comment the lines for 16.04
+
+# For Ubuntu 14.04
+#CUDA_REPO_PKG=http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_8.0.61-1_amd64.deb
+#ML_REPO_PKG=http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1404/x86_64/nvidia-machine-learning-repo-ubuntu1404_4.0-2_amd64.deb
+
+
+
+# For Ubuntu 16.04
+CUDA_REPO_PKG=http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+ML_REPO_PKG=http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/nvidia-machine-learning-repo-ubuntu1604_1.0.0-1_amd64.deb
+
+# Install repo packages
+wget "$CUDA_REPO_PKG" -O /tmp/cuda-repo.deb && sudo dpkg -i /tmp/cuda-repo.deb && rm -f /tmp/cuda-repo.deb
+wget "$ML_REPO_PKG" -O /tmp/ml-repo.deb && sudo dpkg -i /tmp/ml-repo.deb && rm -f /tmp/ml-repo.deb
+
+# Download new list of packages
+sudo apt-get update
