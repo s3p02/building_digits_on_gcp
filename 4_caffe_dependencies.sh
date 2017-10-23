0ce5d6ecbc3915e097212d1f4f99c91a85eed3c9
s3p02 <sreesaisharan.pantangi@ucdenver.edu>
2017-10-23 04:01:53 -0600

    Create 4_caffe_dependencies.sh

---
+++ 4_caffe_dependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install --no-install-recommends build-essential cmake git gfortran libatlas-base-dev libboost-filesystem-dev libboost-python-dev libboost-system-dev libboost-thread-dev libgflags-dev libgoogle-glog-dev libhdf5-serial-dev libleveldb-dev liblmdb-dev libopencv-dev libsnappy-dev python-all-dev python-dev python-h5py python-matplotlib python-numpy python-opencv python-pil python-pip python-pydot python-scipy python-skimage python-sklearn
